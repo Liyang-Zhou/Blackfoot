b22b817cbff449719392511f38aa1db116383d59
LIYANG ZHOU <liyang_zhou_2@sfu.ca>
2021-03-02 00:18:20 -0800

    Updates

---
+++ precache-manifest.f5beaa8120bd647153e33a448793e94d.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f31b6b2aff0270fb1cd206346c24960",
+    "url": "/Blackfoot/index.html"
+  },
+  {
+    "revision": "be6e92898b03b8d436ed",
+    "url": "/Blackfoot/static/css/main.eb20c889.chunk.css"
+  },
+  {
+    "revision": "ade29e067c018df94e94",
+    "url": "/Blackfoot/static/js/2.b2c0d8e3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Blackfoot/static/js/2.b2c0d8e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "240e14aec2117d12b42f",
+    "url": "/Blackfoot/static/js/3.220c49e2.chunk.js"
+  },
+  {
+    "revision": "196f31064c641ef86b22f34244b7ad03",
+    "url": "/Blackfoot/static/js/3.220c49e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be6e92898b03b8d436ed",
+    "url": "/Blackfoot/static/js/main.1205d944.chunk.js"
+  },
+  {
+    "revision": "f2bc456473899380a248",
+    "url": "/Blackfoot/static/js/runtime-main.6e24cc38.js"
+  },
+  {
+    "revision": "72f4cdee411e637e96e1556c0d6b34f4",
+    "url": "/Blackfoot/static/media/apples.72f4cdee.wav"
+  },
+  {
+    "revision": "6da13ad7f534275da62a9309f73a9b9e",
+    "url": "/Blackfoot/static/media/bathroom.6da13ad7.wav"
+  },
+  {
+    "revision": "477b80d7e8f532d136a75f623da14ccf",
+    "url": "/Blackfoot/static/media/chatbot.477b80d7.png"
+  },
+  {
+    "revision": "ab1077cbb8731c2ad399db7109a13351",
+    "url": "/Blackfoot/static/media/cinema.ab1077cb.wav"
+  },
+  {
+    "revision": "6749e3793ea6b6ad09f2758a26e52874",
+    "url": "/Blackfoot/static/media/fish.6749e379.wav"
+  },
+  {
+    "revision": "c5f20e0f4fd92551d99dcb6228eb6cf7",
+    "url": "/Blackfoot/static/media/hello friend.c5f20e0f.wav"
+  },
+  {
+    "revision": "95f0e7ffa0c76e0bd9082cad87fc2cd9",
+    "url": "/Blackfoot/static/media/hello.95f0e7ff.wav"
+  },
+  {
+    "revision": "06fffe151303995406f999cf8cab7dc9",
+    "url": "/Blackfoot/static/media/how are you_.06fffe15.wav"
+  },
+  {
+    "revision": "8907551588e57620584eba0109ac8712",
+    "url": "/Blackfoot/static/media/i will eat.89075515.wav"
+  },
+  {
+    "revision": "48459a24e02c0416cf237de9755c56a4",
+    "url": "/Blackfoot/static/media/kitchen.48459a24.wav"
+  },
+  {
+    "revision": "dbda2f90549d70bd64f7d97d2428ae70",
+    "url": "/Blackfoot/static/media/pngwing.com.dbda2f90.png"
+  },
+  {
+    "revision": "53bb5b08c5750de7105df94bcf581e01",
+    "url": "/Blackfoot/static/media/store.53bb5b08.wav"
+  },
+  {
+    "revision": "96cd63fa815cff621c5a6e360dfd8fe9",
+    "url": "/Blackfoot/static/media/today.96cd63fa.wav"
+  },
+  {
+    "revision": "260892213a974ce124f88fabf013880d",
+    "url": "/Blackfoot/static/media/water.26089221.wav"
+  }
+]);
